2751a382e681554ac00d5dc7a6dd4f43cfb25bd7
Maria Ckopkova <mariakons@mail.ru>
2022-05-29 16:24:42 +0300

    domashka15

---
+++ temp/script15.js
@@ -0,0 +1,37 @@
+/*3. Создать переменные следущих типов: number, string, boolean,
+ undefinded  и вывести их в консоль с помощью команды console.log();
+4. Создать константу и вывести ее в консоль.
+5. Написать три вида циклов: while, do while, for
+6. Факультатитвно: написать оператор swithc case*/
+
+/*переменная типа namber*/ 
+let b = 123;
+console.log(b);
+/*переменная типа string */
+let s = 'здесь будет строка';
+console.log(s);
+/*переменная типа boolean/
+
+/*переменная типа undefinded */
+let c = [
+    "солнце",
+    "ветер",
+    "вода"
+];
+console.log(c[4]);
+/*Константа */
+const P=3.14
+console.log(P);
+let i = 0;
+while (i < 3) {console.log(i);
+i++;
+}
+let a = 0;
+do { 
+    console.log(a);
+a++;
+} while (a < 3);
+for (let j = 0; j < 3; j++) {
+    console.log(j);
+}
+console.log('Ура! Я открыла консоль!');
